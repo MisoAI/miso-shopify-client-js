2660583f274813f56f7892b12c390bf8d0124e2c
ddio <ddio@askmiso.com>
2021-06-25 10:17:02 +0800

    Get api key from url
    MISO-1351

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
-(function () {
-  function reportError (msg) {
+(function (window, document) {
+  const API_KEY = getAPIKey()
+
+  function getAPIKey () {
+    const API_KEY_TOKEN = 'api_key='
+    const me = document.currentScript
+    const urlParser = document.createElement('a')
+    urlParser.href = me.src
+    const searchTokens = urlParser.search.slice(1).split('&')
+    const apiString = searchTokens.find(token => token.startsWith(API_KEY_TOKEN))
+    if (!apiString) {
+      reportError('API Key not found', {})
+      return
+    }
+    const apiTokens = apiString.split('=')
+    if (apiTokens.length !== 2 || !apiTokens[1]) {
+      reportError('Invalid API Key format')
+    }
+    return apiTokens[1]
+  }
+  function reportError (msg, ctx) {
     // TODO: add domain and api key as context
-    console.error(`[ERROR] ${msg}`)
+    console.error(`[ERROR] ${msg}`, ctx || { apiKey: API_KEY })
   }
 
   function genUuid () {
@@ -48,5 +67,10 @@
     return { pageType, resourceId, customerId, anonymousId }
   }
 
-  console.log(setupEnv())
-})()
+  function main () {
+    const ctx = setupEnv()
+    console.log(ctx, API_KEY)
+  }
+
+  main()
+})(window, document)
